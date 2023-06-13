b71183c9a656dd9765bd4afaedd985a7204d0f8a
ilyosbek9531 <iqboljonsheraliyev11@gmail.com>
2023-06-13 16:38:18 +0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import { Main } from 'components/UI/Main/Main'
+import SEO from 'components/SEO'
+
+export default function Home() {
+  return (
+    <>
+      <SEO />
+      <Main />
+    </>
+  )
+}
+
+
